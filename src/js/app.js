53634385b6619f82b5e1fd31b9814712af9870db
Max Thayer <garbados@gmail.com>
2014-03-28 13:13:06 -0400

    initial work on hbs templates

--- src/js/app.js
+++ src/js/app.js
@@ -1,111 +1,74 @@
-var app = angular.module('app', [
-  /* 
-    enable these as you need them
-  */
-  // 'ui.bootstrap.accordion',
-  // 'ui.bootstrap.bindHtml',
-  // 'ui.bootstrap.buttons',
-  // 'ui.bootstrap.carousel',
-  // 'ui.bootstrap.collapse',
-  // 'ui.bootstrap.datepicker',
-  // 'ui.bootstrap.dropdownToggle',
-  // 'ui.bootstrap.modal',
-  // 'ui.bootstrap.pagination',
-  // 'ui.bootstrap.popover',
-  // 'ui.bootstrap.position',
-  // 'ui.bootstrap.progressbar',
-  // 'ui.bootstrap.rating',
-  // 'ui.bootstrap.tabs',
-  // 'ui.bootstrap.timepicker',
-  // 'ui.bootstrap.tooltip',
-  // 'ui.bootstrap.transition',
-  // 'ui.bootstrap.typeahead'
-])
-.constant('chunkSize', 4)
-.constant('apiRoot', '_rewrite/api')
-.constant('imgRoot', '_rewrite/img')
-// MAIN CONTROLLER :O
-.controller('ImgCtrl', [
-  '$scope', 'getImages', 'groupChunk', 'imgRoot',
-  function ($scope, getImages, groupChunk, imgRoot) {
-    getImages()
-    .success(function (res) {
-      var images = res.rows.map(function (image) {
-        image.url = image.src = [imgRoot, image.id].join('/');
-        return image;
-      });
-      
-      var groupchunked = groupChunk(function (image) {
-        var date = new Date(image.key);
-        return new Date(date.getFullYear(), date.getMonth()).getTime();
-      }, images);
+function eggchair () {
+  var apiRoot = '_rewrite';
+  var imgRoot = '_rewrite/img';
 
-      $scope.groups = groupchunked;
-    })
-    .error(function (err) {
-      throw err;
+  function format_res (res) {
+    return res.rows.map(function (row) {
+      return {
+        url: [imgRoot, row.id].join('/'),
+        date: new Date(row.key)
+      };
     });
   }
-])
-.factory('getImages', ['$http', function ($http){
-  return function (){
-    return $http({
-      url: ['_view', 'images'].join('/'),
-      method: 'GET',
-      params: {
-        descending: true
-      }
-    });
-  };
-}])
-// chunk into X equal-ish sized groups
-.factory('chunk', ['chunkSize', function(chunkSize){
-  return function(items){
-    var R = [],
-        rem;
-    for (var i = 0; i < items.length; i++) {
-      rem = i % chunkSize;
-      if (R[rem]) {
-        R[rem].push(items[i]);
-      } else {
-        R[rem] = [items[i]];
-      }
-    }
-    return R;
-  };
-}])
-// group by the value returned by the `getter` function
-.factory('group', function(){
-  return function(getter, items){
+
+  function group_by_date (images) {
     var groups = {};
+
+    images.forEach(function (image) {
+      var date = [image.date.getFullYear(), image.date.getMonth()].join('/');
+
+      if (!groups[date]) {
+        groups[date] = [];
+      }
+
+      groups[date].push(image);
+    });
+
     var groups_sorted = [];
 
-    // group by given field
-    items.forEach(function (item) {
-      var val = getter(item);
-      if (!groups[val]) {
-        groups[val] = [item];
-      } else {
-        groups[val].push(item); 
-      }
-    });
-
-    // convert to array
     Object.keys(groups).forEach(function (key) {
-      groups_sorted.push(groups[key]);
+      var date_parts = key.split('/');
+      var date = new Date(date_parts[0], date_parts[1]);
+      
+      groups_sorted.push({
+        date: date,
+        timestamp: date.getTime(),
+        images: groups[key]
+      });
     });
 
-    // sort, return
-    return groups_sorted.sort(function (a, b) {
-      return getter(b[0]) - getter(a[0]);
+    groups_sorted.sort(function (a, b) {
+      return b.timestamp - a.timestamp;
     });
-  };
-})
-// chunk and group items using the given `field`
-.factory('groupChunk', ['chunk', 'group', function(chunk, group){
-  return function(getter, items){
-    return group(getter, items).map(function(page){
-      return chunk(page);
+
+    return groups_sorted;
+  }
+
+  function load_images (done) {
+    var url = [apiRoot, '_view', 'images'].join('/');
+    $.getJSON(url, {
+      descending: true
+    }, function (res) {
+      var images = format_res(res);
+      var groups = group_by_date(images);
+
+      done(groups);
     });
-  };
-}]);
+  }
+
+  function render_templates (header, groups) {
+    load_images(function (groups) {
+      var header_template = Handlebars.templates['title.hbs'](header);
+      $('#title').html(header_template);
+
+      var groups_template = Handlebars.templates['list.hbs']({ groups: groups });
+      $('#images').html(groups_template);
+    });
+  }
+
+  load_images(render_templates.bind(null, {
+    title: 'eggchair'
+  }));
+}
+
+$(eggchair);
