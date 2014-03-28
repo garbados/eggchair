466f44902f2db3e5d90ce3458f915429ee0668b5
Max Thayer <garbados@gmail.com>
2014-03-28 15:19:04 -0400

    list functions ftw

--- app.js
+++ app.js
@@ -9,15 +9,102 @@ ddoc = {
         if (doc._id.indexOf('_design') === 0) {
           // ignore
         } else if (doc._attachments.file.content_type.indexOf('image') === 0) {
-          var date = new Date(doc.timestamp),
-              year = date.getFullYear(),
-              month = date.getMonth();
-          emit(doc.timestamp, null);
+          emit(-doc.timestamp, null);
         }
       }
     }
   },
-  lists: {},
+  lists: {
+    groupchunk: function (head, req) {
+      var chunkSize = req.query.chunkSize || 4;
+      var months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ];
+
+      function format_row (row) {
+        return {
+          url: row.id,
+          date: new Date(-row.key)
+        };
+      }
+
+      function chunk (items){
+        var R = [],
+            rem;
+        for (var i = 0; i < items.length; i++) {
+          rem = i % chunkSize;
+          if (R[rem]) {
+            R[rem].push(items[i]);
+          } else {
+            R[rem] = [items[i]];
+          }
+        }
+        return R;
+      }
+
+      function group_by_date (images) {
+        var groups = {};
+
+        images.forEach(function (image) {
+          var date = [image.date.getFullYear(), image.date.getMonth()].join('/');
+
+          if (!groups[date]) {
+            groups[date] = [];
+          }
+
+          groups[date].push(image);
+        });
+
+        var groups_sorted = [];
+
+        Object.keys(groups).forEach(function (key) {
+          var date_parts = key.split('/');
+          var date = new Date(date_parts[0], date_parts[1]);
+          var images = groups[key].map(function (image) {
+            return image.url;
+          });
+          
+          groups_sorted.push({
+            date: [months[date_parts[1]], date_parts[0]].join(', '),
+            timestamp: date.getTime(),
+            chunks: chunk(images).map(function (image_chunk) {
+              return {
+                images: image_chunk
+              };
+            })
+          });
+        });
+
+        groups_sorted.sort(function (a, b) {
+          return b.timestamp - a.timestamp;
+        });
+
+        return groups_sorted;
+      }
+
+      var row = getRow();
+      var images = [];
+      while (row) {
+        images.push(format_row(row));
+        row = getRow();
+      }
+
+      images.reverse();
+      var groups = group_by_date(images);
+      send(JSON.stringify(groups));
+    }
+  },
   shows: {},
   rewrites: require('./rewrites.json')
 };
--- src/js/app.js
+++ src/js/app.js
@@ -2,112 +2,28 @@ function eggchair (config) {
   var pathRoot = (location.pathname.indexOf('_rewrite') === -1) ? '' : '_rewrite';
   var apiRoot = pathRoot;
   var imgRoot = pathRoot ? pathRoot + '/img' : 'img';
-  var chunkSize = 4;
   var header = config.header;
 
-  var months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ];
-
-  function format_res (res) {
-    return res.rows.map(function (row) {
-      return {
-        url: [imgRoot, row.id].join('/'),
-        date: new Date(row.key)
-      };
-    });
-  }
-
-  function chunk (items){
-    var R = [],
-        rem;
-    for (var i = 0; i < items.length; i++) {
-      rem = i % chunkSize;
-      if (R[rem]) {
-        R[rem].push(items[i]);
-      } else {
-        R[rem] = [items[i]];
-      }
-    }
-    return R;
-  }
-
-  function group_by_date (images) {
-    var groups = {};
-
-    images.forEach(function (image) {
-      var date = [image.date.getFullYear(), image.date.getMonth()].join('/');
-
-      if (!groups[date]) {
-        groups[date] = [];
-      }
-
-      groups[date].push(image);
-    });
-
-    var groups_sorted = [];
-
-    Object.keys(groups).forEach(function (key) {
-      var date_parts = key.split('/');
-      var date = new Date(date_parts[0], date_parts[1]);
-      var images = groups[key].map(function (image) {
-        return image.url;
-      });
-      
-      groups_sorted.push({
-        date: [months[date_parts[1]], date_parts[0]].join(', '),
-        timestamp: date.getTime(),
-        chunks: chunk(images).map(function (image_chunk) {
-          return {
-            images: image_chunk
-          };
-        })
-      });
-    });
-
-    groups_sorted.sort(function (a, b) {
-      return b.timestamp - a.timestamp;
-    });
-
-    return groups_sorted;
-  }
-
   function load_images (done) {
-    var url = [apiRoot, '_view', 'images'].join('/');
-    $.getJSON(url, {
-      descending: true
-    }, function (res) {
-      var images = format_res(res);
-      var groups = group_by_date(images);
-
-      done(groups);
-    });
+    var url = [apiRoot, '_list', 'groupchunk', 'images'].join('/');
+    $.getJSON(url + location.search, done);
   }
 
   function render_templates (groups) {
-    load_images(function (groups) {
-      var header_template = Handlebars.templates.title(header);
-      $('#title').html(header_template);
+    var header_template = Handlebars.templates.title(header);
+    $('#title').html(header_template);
 
-      var groups_template = Handlebars.templates.list({ groups: groups });
-      $('#images').html(groups_template);
+    var groups_template = Handlebars.templates.list({ 
+      groups: groups,
+      imgRoot: imgRoot
     });
+
+    $('#images').html(groups_template);
   }
 
   load_images(render_templates);
 }
 
 $(eggchair.bind(null, {
-  header: header
+  header: header || {}
 }));
