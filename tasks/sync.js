698a05ee05d2d9f68191e72537bc84f4a0299790
Max Thayer <garbados@gmail.com>
2014-04-28 18:20:31 -0400

    sync fix

--- tasks/sync.js
+++ tasks/sync.js
@@ -3,12 +3,12 @@ var quilter = require('quilter');
 try {
   var config = require('../config');
 } catch (e) {
-  console.log('Please run `npm run-script config` first');
+  console.log('Please run `npm run config` first');
 }
 
 if (config) {
   quilter.push({
     local: config.src,
     remote: config.db
-  });
+  }, function () {});
 }
